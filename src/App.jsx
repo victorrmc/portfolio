5602038cbba047b3bbf9187da3f25d4e287da5cf
victormc <victortenemc@gmail.com>
2023-10-04 13:15:01 +0100

    Añadiendo componente de Cardexperiencia y añadida experiencia CIAC

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,49 @@ import { Header } from "./components/Header";
 import { Inicio } from "./components/Inicio";
 import { SobreMi } from "./components/SobreMi";
 
-const scrTecnologiasAshotel = ["Typescript_logo_2020.svg", "PHP-logo.svg", "HTML5_Badge.svg", "CSS3_logo.svg", "Wordpress-Logo.svg"]
+const logoTecAshotel = [
+  {
+    url: "Typescript_logo_2020.svg",
+    title: "Typescript",
+  },
+  {
+    url: "PHP-logo.svg",
+    title: "PHP",
+  },
+  {
+    url: "HTML5_Badge.svg",
+    title: "HTML5",
+  },
+  {
+    url: "CSS3_logo.svg",
+    title: "CSS3",
+  },
+  {
+    url: "Wordpress-Logo.svg",
+    title: "Wordpress",
+  }
+];
+const logoTecCIAC = [
+  {
+    url: "c--4.svg",
+    title: "C#"
+  },
+  {
+    url: "Xamarin-logo.svg",
+    title: "Xamarin"
+  }
+]
+
+const parrafosAshotel = ["En mi rol en Ashotel, desempeñé un papel Full Stack. Primero, en el área de desarrollo web, trabajé en la actualización y creación de nuevas páginas en la sección de Observatorio, utilizando WordPress con backend en PHP para mejorar la experiencia en línea",
+ "Además, en el ámbito de la automatización y gestión de datos, utilicéTypeScript para crear automatizaciones en Excel, mejorando la eficiencia de los flujos de trabajo. También implementé automatizaciones con Microsoft Power Automate para simplificar procesos internos.",
+ "Finalmente, lideré la creación de un MVP (Producto Mínimo Viable) en Power Apps, lo que contribuyó a la optimización de tareas y a una mayor eficiencia operativa en Ashotel."
+]
+const parrafosCIAC = ["Como Desarrollador Xamarin en CIAC, optimicé la velocidad y eficiencia de las listas desplegables en nuestra aplicación mediante técnicas de lazy loading y almacenamiento en caché",
+"Además, lideré la creación exitosa de una nueva sección de la aplicación, mejorando la experiencia de usuario."
+]
+
+
+
 function App() {
   return (
     <>
@@ -18,7 +60,8 @@ function App() {
           <SobreMi/>
           <article className="flex flex-col my-5 space-y-3 justify-items-start ">
             <h3 className="text-3xl text-white text-center">Experiencia</h3>
-             <CardExperiencia cargo="Programador Full Stack" empresa="Ashotel · Contrato de prácticas" fecha="mar. 2023 - may. 2023 · 3 meses" srcTencologias={scrTecnologiasAshotel} />
+             <CardExperiencia cargo="Programador Full Stack" empresa="Ashotel · Contrato de prácticas" fecha="mar. 2023 - may. 2023 · 3 meses" srcTencologias={logoTecAshotel} parrafos={parrafosAshotel}/>
+             <CardExperiencia cargo="Desarrollador Xamarin" empresa="CENTRO DE INFORMATICA Y ANALISIS DE CANARIAS SL · Contrato de prácticas" fecha="mar. 2022 - may. 2022 · 3 meses" srcTencologias={logoTecCIAC} parrafos={parrafosCIAC}/>
             <div className="w-96 bg-slate-200 h-96"></div>
           </article>
         </section>
--- src/components/CardExperiencia.jsx
+++ src/components/CardExperiencia.jsx
@@ -1,83 +1,31 @@
-export function CardExperiencia({ cargo, empresa, fecha, srcTencologias }){
-    return(
-        <div className="lg:flex mx-4 h-auto w-auto bg-slate-800  space-y-6 lg:space-y-0 rounded-3xl p-5">
-              <div className="lg:flex-initial grid text-center  lg:text-left lg:content-center space-y-2 mr-3">
-                <p className="text-xl text-yellow-300 ">
-                  {cargo}
-                </p>
-                <p className="text-lg text-blue-300">
-                  {empresa}
-                </p>
-                <p className="font-mono text-white">
-                  {fecha}
-                </p>
-                <p className=" pt-3 font-bold text-white">
-                  Tecnologías utilizadas:
-                </p>
-                <ul className="flex items-center justify-center space-x-2">
-                  <li>
-                    <img
-                      className="w-10"
-                      src="Typescript_logo_2020.svg"
-                      alt="Logo Typescript"
-                      title="Typescript"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      className="w-14"
-                      src="PHP-logo.svg"
-                      alt="Logo PHP"
-                      title="PHP"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      className="w-10"
-                      src="HTML5_Badge.svg"
-                      alt="Logo HTML5"
-                      title="HTML5"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      className="w-10"
-                      src="CSS3_logo.svg"
-                      alt="Logo CSS3"
-                      title="CSS3"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      className="w-10"
-                      src="Wordpress-Logo.svg"
-                      alt="Logo HTML5"
-                      title="HTML5"
-                    />
-                  </li>
-                </ul>
-              </div>
-              <div className="lg:flex-1 space-y-3 s ">
-                <p className="text-white">
-                  En mi rol en Ashotel, desempeñé un papel Full Stack. Primero,
-                  en el área de desarrollo web, trabajé en la actualización y
-                  creación de nuevas páginas en la sección de Observatorio,
-                  utilizando WordPress con backend en PHP para mejorar la
-                  experiencia en línea.
-                </p>
-                <p className="text-white">
-                  Además, en el ámbito de la automatización y gestión de datos,
-                  utilicé TypeScript para crear automatizaciones en Excel,
-                  mejorando la eficiencia de los flujos de trabajo. También
-                  implementé automatizaciones con Microsoft Power Automate para
-                  simplificar procesos internos.
-                </p>
-                <p className="text-white">
-                  Finalmente, lideré la creación de un MVP (Producto Mínimo
-                  Viable) en Power Apps, lo que contribuyó a la optimización de
-                  tareas y a una mayor eficiencia operativa en Ashotel.
-                </p>
-              </div>
-            </div>
-    )
+export function CardExperiencia({ cargo, empresa, fecha, srcTencologias, parrafos }) {
+  return (
+    <div className="lg:flex mx-4 h-auto w-auto bg-slate-800  space-y-6 lg:space-y-0 rounded-3xl p-5">
+      <div className=" grid text-center lg:flex-1 lg:text-left lg:content-center space-y-2 mr-3 ">
+        <p className="text-xl text-yellow-300 ">{cargo}</p>
+        <p className="text-lg text-blue-300 text-balance">{empresa}</p>
+        <p className="font-mono text-white">{fecha}</p>
+        <p className=" pt-3 font-bold text-white">Tecnologías utilizadas:</p>
+        <ul className="flex flex-1 items-center justify-center space-x-2 ">
+          {srcTencologias.map((tecnologias, index) => (
+            <li key={index}>
+              <img
+                className="h-10"
+                src={tecnologias.url}
+                alt={'Logo de '+tecnologias.title}
+                title={tecnologias.title}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className=" space-y-3 grid lg:content-center lg:max-w-xl ">
+        {parrafos.map((element, index) => (
+          <p key={index} className="text-white">
+            {element}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
