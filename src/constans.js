fcb8f0cc2461e73ffcae010b3829594e662c4ffe
Victor Marrero Carrillo <victortenemc@gmail.com>
2025-07-02 22:56:46 +0100

    Add expo and modifiy bitbox tex

--- src/constans.js
+++ src/constans.js
@@ -1,228 +1,231 @@
 export const logoTecAshotel = [
-    {
-        url: "Typescript.svg",
-    },
-    {
-        url: "PHP.svg",
-    },
-    {
-        url: "HTML5.svg",
-    },
-    {
-        url: "CSS3.svg",
-    },
-    {
-        url: "Wordpress.svg",
-    },
+  {
+    url: "Typescript.svg",
+  },
+  {
+    url: "PHP.svg",
+  },
+  {
+    url: "HTML5.svg",
+  },
+  {
+    url: "CSS3.svg",
+  },
+  {
+    url: "Wordpress.svg",
+  },
 ];
 export const logoTeBitbox = [
-    {
-        url: "Java.svg",
-    },
-    {
-        url: "SpringBoot.svg",
-    },
-    {
-        url: "JavaScript.svg",
-    },
-    {
-        url: "React.svg",
-    },
-    {
-        url: "React.svg",
-        title: "React Native"
-    },
-    {
-        url: "Git.svg",
-    },
-
+  {
+    url: "Java.svg",
+  },
+  {
+    url: "SpringBoot.svg",
+  },
+  {
+    url: "JavaScript.svg",
+  },
+  {
+    url: "React.svg",
+  },
+  {
+    url: "Expo.svg",
+    title: "Expo",
+  },
+  {
+    url: "Git.svg",
+  },
 ];
 export const logoTecCIAC = [
-    {
-        url: "CSharp.svg",
-        title: "C#",
-    },
-    {
-        url: "Xamarin.svg",
-    },
+  {
+    url: "CSharp.svg",
+    title: "C#",
+  },
+  {
+    url: "Xamarin.svg",
+  },
 ];
 
 export const logoTecOferAlert = [
-    {
-        url: "Python.svg",
-    },
-    {
-        url: "Flask.svg",
-    },
-    {
-        url: "Celery.svg",
-    },
+  {
+    url: "Python.svg",
+  },
+  {
+    url: "Flask.svg",
+  },
+  {
+    url: "Celery.svg",
+  },
 ];
 
 export const logoDAW = [
-    {
-        url: "React.svg",
-    },
-    {
-        url: ".NET.svg",
-    },
-    {
-        url: "Spring.svg",
-    },
-    {
-        url: "Bootstrap.svg",
-    },
-    {
-        url: "JavaScript.svg",
-    },
-    {
-        url: "Java.svg",
-    },
-    {
-        url: "CSharp.svg",
-        title: "C#",
-    },
-    {
-        url: "SQL.svg",
-    },
-    {
-        url: "HTML5.svg",
-    },
-    {
-        url: "CSS3.svg",
-    },
+  {
+    url: "React.svg",
+  },
+  {
+    url: ".NET.svg",
+  },
+  {
+    url: "Spring.svg",
+  },
+  {
+    url: "Bootstrap.svg",
+  },
+  {
+    url: "JavaScript.svg",
+  },
+  {
+    url: "Java.svg",
+  },
+  {
+    url: "CSharp.svg",
+    title: "C#",
+  },
+  {
+    url: "SQL.svg",
+  },
+  {
+    url: "HTML5.svg",
+  },
+  {
+    url: "CSS3.svg",
+  },
 ];
 export const logoDAM = [
-    {
-        url: "Java.svg",
-    },
-    {
-        url: "CSharp.svg",
-        title: "C#",
-    },
-    {
-        url: "PHP.svg",
-    },
-    {
-        url: "SQL.svg",
-    },
-    {
-        url: "HTML5.svg",
-    },
-    {
-        url: "CSS3.svg",
-    },
-    {
-        url: "Unity.svg",
-    },
-    {
-        url: "AndroidStudio.svg",
-        title: "Android Studio",
-    },
+  {
+    url: "Java.svg",
+  },
+  {
+    url: "CSharp.svg",
+    title: "C#",
+  },
+  {
+    url: "PHP.svg",
+  },
+  {
+    url: "SQL.svg",
+  },
+  {
+    url: "HTML5.svg",
+  },
+  {
+    url: "CSS3.svg",
+  },
+  {
+    url: "Unity.svg",
+  },
+  {
+    url: "AndroidStudio.svg",
+    title: "Android Studio",
+  },
 ];
 export const logoFrameworks = [
-    {
-        url: "React.svg",
-    },
-    {
-        url: ".NET.svg",
-    },
-    {
-        url: "Spring.svg",
-    },
-    {
-        url: "Tailwind.svg",
-    },
-    {
-        url: "Bootstrap.svg",
-    },
+  {
+    url: "React.svg",
+  },
+  {
+    url: ".NET.svg",
+  },
+  {
+    url: "Spring.svg",
+  },
+  {
+    url: "Tailwind.svg",
+  },
+  {
+    url: "Bootstrap.svg",
+  },
+  {
+    url: "Expo.svg",
+    title: "Expo",
+  },
 ];
 
 export const logoLenguajes = [
-    {
-        url: "JavaScript.svg",
-    },
-    {
-        url: "Java.svg",
-    },
-    {
-        url: "CSharp.svg",
-        title: "C#",
-    },
-    {
-        url: "Python.svg",
-    },
-    {
-        url: "PHP.svg",
-    },
-    {
-        url: "SQL.svg",
-    },
-    {
-        url: "HTML5.svg",
-    },
-    {
-        url: "CSS3.svg",
-    },
+  {
+    url: "JavaScript.svg",
+  },
+  {
+    url: "Java.svg",
+  },
+  {
+    url: "CSharp.svg",
+    title: "C#",
+  },
+  {
+    url: "Python.svg",
+  },
+  {
+    url: "PHP.svg",
+  },
+  {
+    url: "SQL.svg",
+  },
+  {
+    url: "HTML5.svg",
+  },
+  {
+    url: "CSS3.svg",
+  },
 ];
 
 export const logoIDEs = [
-    {
-        url: "VisualStudioCode.svg",
-        title: "Visual Studio Code",
-    },
-    {
-        url: "VisualStudio2022.svg",
-        title: "Visual Studio 2022",
-    },
-    {
-        url: "ApacheNetBeans.svg",
-        title: "Apache NetBeans",
-    },
-    {
-        url: "IntelliJIDEA.svg",
-        title: "IntelliJ IDEA",
-    },
-    {
-        url: "AndroidStudio.svg",
-        title: "Android Studio",
-    },
-    {
-        url: "Unity.svg",
-    },
-]
+  {
+    url: "VisualStudioCode.svg",
+    title: "Visual Studio Code",
+  },
+  {
+    url: "VisualStudio2022.svg",
+    title: "Visual Studio 2022",
+  },
+  {
+    url: "ApacheNetBeans.svg",
+    title: "Apache NetBeans",
+  },
+  {
+    url: "IntelliJIDEA.svg",
+    title: "IntelliJ IDEA",
+  },
+  {
+    url: "AndroidStudio.svg",
+    title: "Android Studio",
+  },
+  {
+    url: "Unity.svg",
+  },
+];
 export const logoHerramientas = [
-    {
-        url: "Git.svg",
-    },
-    {
-        url: "Github.svg",
-    },
-    {
-        url: "Postman.svg",
-    },
-    {
-        url: "Docker.svg",
-    },
-    {
-        url: "Dolibarr.svg",
-    },
+  {
+    url: "Git.svg",
+  },
+  {
+    url: "Github.svg",
+  },
+  {
+    url: "Postman.svg",
+  },
+  {
+    url: "Docker.svg",
+  },
+  {
+    url: "Dolibarr.svg",
+  },
 ];
 
 export const parrafosAshotel = [
-    "Desarrollo y actualización de páginas web en WordPress con backend en PHP mejorando la experiencia de usuario.",
-    "Automatización de tareas mejorando la productividad de mis compañeros utilizando TypeScript para crear automatizaciones en Excel y Microsoft Power Automate con el fin de simplificar procesos internos.",
-    "Adopción del rol de líder durante el desarrollo de un Producto Mínimo Viable (MVP) en Power Apps, con el objetivo de realizar una optimización de tareas y aumento de la eficiencia operativa en la empresa.",
+  "Desarrollo y actualización de páginas web en WordPress con backend en PHP mejorando la experiencia de usuario.",
+  "Automatización de tareas mejorando la productividad de mis compañeros utilizando TypeScript para crear automatizaciones en Excel y Microsoft Power Automate con el fin de simplificar procesos internos.",
+  "Adopción del rol de líder durante el desarrollo de un Producto Mínimo Viable (MVP) en Power Apps, con el objetivo de realizar una optimización de tareas y aumento de la eficiencia operativa en la empresa.",
 ];
 export const parrafosBitbox = [
-    "Como desarrollador en Bitboxer me he encargado de hacer análisis y desarrollar aplicaciones tanto en el ERP como en diferentes proyectos, actualmente estoy fijo en el proyecto de Warehouse.",
-    "He trabajado en equipo a través de herramientas como Git y Subversion, desarrollando con tecnologías como Java, JavaScript, React, React Native y Spring Boot.",
-    "También he realizado modificaciones en las librerías y pasado por todo el proceso de desarrollo: análisis, documentación, desarrollo, y resolución de problemas."
+  "Como desarrollador en Bitboxer me he encargado de hacer análisis y desarrollar partes de las aplicaciones tanto en el ERP como en diferentes proyectos. ",
+  "He trabajado en equipo a través de herramientas como Git y Subversion, desarrollando con tecnologías como Java, JavaScript, React, Expo y Spring Boot.",
+  "También he realizado modificaciones en las librerías y pasado por todo el proceso de desarrollo: análisis, documentación, desarrollo, y resolución de problemas.",
 ];
 export const parrafosCIAC = [
-    "Optimización de la velocidad y eficiencia de las listas desplegables en nuestra aplicación mediante técnicas de carga, lazy load y almacenamiento en caché.",
-    "Adopción del rol de líder en la creación exitosa de una nueva sección de la aplicación, creando nuevas funciones a esta.",
+  "Optimización de la velocidad y eficiencia de las listas desplegables en nuestra aplicación mediante técnicas de carga, lazy load y almacenamiento en caché.",
+  "Adopción del rol de líder en la creación exitosa de una nueva sección de la aplicación, creando nuevas funciones a esta.",
 ];
 export const parrafosOferAlert = [
-    "Aplicación web de seguimiento de ofertas en tiempo real. Con OfertAlert, puedes ingresar un enlace y recibir notificaciones por correo electrónico cada vez que el artículo asociado esté en oferta.",
+  "Aplicación web de seguimiento de ofertas en tiempo real. Con OfertAlert, puedes ingresar un enlace y recibir notificaciones por correo electrónico cada vez que el artículo asociado esté en oferta.",
 ];
